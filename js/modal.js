1f9a2c9d11ddf2d1e74b2c482f021c21d2f99e8c
playsoksana <playsoksana@gmail.com>
2021-02-01 17:02:31 +0200

    набрасывает меню

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,16 @@
     refs.modal.classList.toggle('backdrop--hidden');
   }
 })();
+(() => {
+  const menuBtnRef = document.querySelector ("[data-menu-button]");
+  const mobileMenuRef = document.querySelector ("[data-menu]");
+  menuBtnRef.addEventListener("click", () =>  {
+const expended = 
+menuBtnRef.getAttribute("aria-expended") === "true" || false;
+menuBtnRef.classList.toggle("is-open");
+menuBtnRef.setAttribute("aria-expended", !expended);
+mobileMenuRef.classList.toggle("is-open");
+  });
+}
+)();
+
