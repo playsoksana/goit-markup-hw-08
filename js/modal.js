1089249b43173ad5d18e550bb776181b65444e0c
playsoksana <playsoksana@gmail.com>
2021-02-06 01:32:22 +0200

    Добавляет меню мобильного

--- js/modal.js
+++ js/modal.js
@@ -12,16 +12,32 @@
     refs.modal.classList.toggle('backdrop--hidden');
   }
 })();
+
+
 (() => {
-  const menuBtnRef = document.querySelector ("[data-menu-button]");
-  const mobileMenuRef = document.querySelector ("[data-menu]");
-  menuBtnRef.addEventListener("click", () =>  {
-const expended = 
-menuBtnRef.getAttribute("aria-expended") === "true" || false;
-menuBtnRef.classList.toggle("is-open");
-menuBtnRef.setAttribute("aria-expended", !expended);
-mobileMenuRef.classList.toggle("is-open");
-  });
-}
-)();
+  const refs = {
+    openModalBtn: document.querySelector('[data-open-menu]'),
+    closeModalBtn: document.querySelector('[data-close-menu]'),
+    modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--hidden');
+  }
+})();
+// (() => {
+//   const menuBtnRef = document.querySelector ("[data-menu-button]");
+//   const mobileMenuRef = document.querySelector ("[data-menu]");
+//   menuBtnRef.addEventListener("click", () =>  {
+// const expended = 
+// menuBtnRef.getAttribute("aria-expended") === "true" || false;
+// menuBtnRef.classList.toggle("is-open");
+// menuBtnRef.setAttribute("aria-expended", !expended);
+// mobileMenuRef.classList.toggle("is-open");
+//   });
+// }
+// )();
 
