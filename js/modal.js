cac9008c9aa020461f00a42164f612b349793541
playsoksana <playsoksana@gmail.com>
2021-02-06 23:55:56 +0200

    Исправляет скрипт меню футера
    по дефелту спрятан

--- js/modal.js
+++ js/modal.js
@@ -29,19 +29,3 @@
       setup("menu");
   }
 )();
-
-    (() => {
-   const setup = (name) => {
-        const backdrop = document.querySelector(`[data-backdrop-${name}]`);
-        const buttons = document.querySelectorAll(
-          `[data-toggle-backdrop-${name}]`
-        );
-        const toggleBackdrop = () => backdrop.classList.toggle("menu--hidden2");
-        buttons.forEach((button) =>
-          button.addEventListener("click", toggleBackdrop)
-        );
-      };
-      setup("schedule2");
-      setup("menu2");
-  }
-)();
