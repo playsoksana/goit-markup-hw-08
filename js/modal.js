37924aab2fb399cec21fce5e3becb25ceb6ef2e9
playsoksana <playsoksana@gmail.com>
2021-02-07 08:27:38 +0200

    Добавляет js для мобильного меню

--- js/modal.js
+++ js/modal.js
@@ -12,20 +12,3 @@
     refs.modal.classList.toggle('backdrop--hidden');
   }
 })();
-
-
-    (() => {
-   const setup = (name) => {
-        const backdrop = document.querySelector(`[data-backdrop-${name}]`);
-        const buttons = document.querySelectorAll(
-          `[data-toggle-backdrop-${name}]`
-        );
-        const toggleBackdrop = () => backdrop.classList.toggle("menu--hidden");
-        buttons.forEach((button) =>
-          button.addEventListener("click", toggleBackdrop)
-        );
-      };
-      setup("schedule");
-      setup("menu");
-  }
-)();
